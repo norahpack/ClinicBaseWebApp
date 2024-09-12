2eb882b28fb8d4a2d8244a05b0f266ff3fd3a8ab
norahpack <norahpack14@gmail.com>
2024-09-11 22:16:26 -0700

    first commit

--- project/js/main.js
+++ project/js/main.js
@@ -1,9 +1,5 @@
 function showPicture(){
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
-  $("p").html("High five! You're building your first web app!");
-
-  // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
-  
+  // dollar sign = document.getElementById
+  $("#image").append('<img class="rounded-circle" src="images/hmclogo.png"/>');
+  $("p").html("We are ready to build a scleroderma app!");  
 }
